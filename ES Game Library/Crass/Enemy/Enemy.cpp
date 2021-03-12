54e77ce026420445030fb537f7dc2f7298cbdcec
木幡 光秀 <takeminakatanomikoto71@gmail.com>
2021-03-12 18:57:57 +0900

    敵移動、EnemyManager/EnemyBase作成

--- ES Game Library/Crass/Enemy/Enemy.cpp
+++ ES Game Library/Crass/Enemy/Enemy.cpp
@@ -11,15 +11,27 @@ Enemy::~Enemy()
 
 bool Enemy::Initialize()
 {
+	_model		= GraphicsDevice.CreateModelFromFile(_T("kariMan_1.X"));
+//	_collision  = 
+	_position =  Vector3(0, 0, 1);
+	_angle	  =  Vector3(0, 0, 0);
+	_model->SetPosition(_position);
+	_model->SetRotation(_angle);
+	_hp = 10;
 	return true;
 }
 
 int Enemy::Update()
 {
+	Move();
 	return 0;
 }
 
 void Enemy::Draw()
 {
+	_model->Draw();
+}
 
+void Enemy::Move() {
+	_position.z -= 0.5f;
 }
--- ES Game Library/Crass/Enemy/EnemyManeger.cpp
+++ ES Game Library/Crass/Enemy/EnemyManeger.cpp
@@ -3,25 +3,51 @@
 
 EnemyManeger::EnemyManeger()
 {
-
+	_enemy.clear();
 }
 
 EnemyManeger::~EnemyManeger()
 {
-
+	for (int i = 0; i < _enemy.size() - 1; i++)
+	{
+		delete _enemy[i];
+	}
 }
 
 bool EnemyManeger::Initialize()
 {
+
 	return true;
 }
 
 int EnemyManeger::Update()
 {
+	time++;
+	Generate();
+	for (int i = 0; i < _enemy.size() - 1; i++)
+	{
+		_enemy[i]->Update();
+	}
 	return 0;
 }
 
 void EnemyManeger::Draw()
 {
+	for (int i = 0; i < _enemy.size() - 1; i++)
+	{
+		_enemy[i]-> Draw();
+	}
+}
+
+void EnemyManeger::Generate() {
+	if (time >= 100)
+	{
+		_enemy.push_back(new Enemy);
+		_enemy[_enemy.size()]->Initialize();
+		time = 0;
+	}
+}
+
+void EnemyManeger::OnCollisionEnter() {
 
 }
